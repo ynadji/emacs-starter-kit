c79e552f4bf171516f1f6f50ac939aba5ae44281
Yacin Nadji <yacin@gatech.edu>
2009-11-09 22:04:06 -0500

    finally clojure works again. i have to get work done, but go back and clean this up. you should probably make this a subproject of config.

--- yacin/clojure.el
+++ yacin/clojure.el
@@ -1,3 +1,6 @@
 ;;;; .emacs-esque additions
 ; clojure
 (clojure-slime-config)
+
+(setq swank-clojure-classpath (mapcar (lambda (p) (expand-file-name p)) (split-string (getenv "CLASSPATH") ":")))
+(setq swank-clojure-extra-vm-args (list "-server" "-Xmx1024m"))
